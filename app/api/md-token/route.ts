1e35a040a39f7fb0a8f71038b7722019fe7cd20c
Casey Haber <haber.casey@gmail.com>
2025-02-15 21:30:02 -0800

    Changing token scaling

--- app/api/md-token/route.ts
+++ app/api/md-token/route.ts
@@ -1,11 +1,22 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import createReadScalingToken from './createReadScalingToken';
 
 
-export async function GET(_request: NextRequest) {
-    // The token provided to the frontend permits read-write access to data in the MotherDuck account.
-    // Switch to MOTHERDUCK_READ_SCALING_TOKEN for a token with eventually consistent read-only access.
-    // To generate short-lived tokens for use in the frontend, see the 
-    // MotherDuck API documentation: https://api.motherduck.com/docs/
-    return NextResponse.json({ mdToken: process.env.MOTHERDUCK_TOKEN || '' });
+export async function GET(request: NextRequest) {
+    try {
+        if (request.nextUrl.searchParams.get("read_scaling") === "true" && process.env.NODE_ENV !== 'development') {
+            const { token, expire_at } = await createReadScalingToken(3600);
+            return NextResponse.json({ mdToken: token, expire_at });
+        }
+
+        return NextResponse.json({ mdToken: process.env.MOTHERDUCK_TOKEN || '', expire_at: '' });
+
+    } catch (error) {
+        console.error('Failed to generate token', error);
+        return NextResponse.json(
+            { error: `Failed to generate token` },
+            { status: 500 }
+        );
+    }
 }
