e5fefa738f6b42ce3b72650d9511b4dfe122a62d
Casey Haber <haber.casey@gmail.com>
2025-02-15 21:32:47 -0800

    Fix type

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ function CustomerOrdersTable() {
     const { fetchCustomerOrdersData, error } = useFetchCustomerOrdersData();
     // const [customerOrdersData, setCustomerOrdersData] = useState<{ username: string, email: string, totalAmount: number, orderDate: string }[]>([]);
     const [loading, setLoading] = useState(false);
-    const [customerOrdersData, setCustomerOrdersData] = useState<any[]>([]);
+    const [customerOrdersData, setCustomerOrdersData] = useState<object[]>([]);
     const handleFetchCustomerOrdersData = async () => {
         setLoading(true);
         const result = await fetchCustomerOrdersData();
